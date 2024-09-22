2c13a1cce4a508f18b32feb397e04bb701944011
Ahmed Fareed <ahmed@techchaps.com>
2024-09-22 13:56:44 +0100

    chore: add .env

--- app/models.py
+++ app/models.py
@@ -3,9 +3,13 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Define the SQLite URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create the database engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
